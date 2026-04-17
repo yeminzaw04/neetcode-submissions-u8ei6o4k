0d1488449fb28b60287de2de11423017d3597068
yeminzaw04 <42385092+yeminzaw04@users.noreply.github.com>
2026-04-16 22:56:24 -0700

    Add: buy-and-sell-crypto - submission-18

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-18.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0; // Buy
+        let right = 1; // Sell
+        let maxProfit = 0; // Record profit
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                let profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
