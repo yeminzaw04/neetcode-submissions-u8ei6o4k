5233deec2e7424cb7da3ef93bf4cf9823ff3013d
yeminzaw04 <42385092+yeminzaw04@users.noreply.github.com>
2026-04-18 12:38:36 -0700

    Add: longest-substring-without-duplicates - submission-28

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-28.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let left = 0;
+        let right = 0;
+        let maxLength = 0;
+        let mySub = new Set();
+
+        while (right < s.length) {
+            if (!mySub.has(s[right])) {
+                mySub.add(s[right]);
+                right++;
+            } else {
+                mySub.delete(s[left]);
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left)
+        }
+
+        return maxLength;
+    }
+}
