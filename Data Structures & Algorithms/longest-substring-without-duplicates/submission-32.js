c775f14bd944c4e92124ce94f2faf7b5da6cc252
yeminzaw04 <42385092+yeminzaw04@users.noreply.github.com>
2026-04-18 13:10:05 -0700

    Add: longest-substring-without-duplicates - submission-32

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-32.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let left = 0;
+        let maxLength = 0;
+        let charSet = new Set();
+
+        for (let right = 0; right < s.length; right++) {
+            while (charSet.has(s[right])) {
+                charSet.delete(s[left]);
+                left++;
+            }
+            charSet.add(s[right]);
+            maxLength = Math.max(maxLength, right - left + 1)
+        }
+
+        return maxLength;
+    }
+}
