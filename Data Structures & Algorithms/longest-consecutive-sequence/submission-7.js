b9114000af2c19abd73a6f5b355651780b889cc7
yeminzaw04 <42385092+yeminzaw04@users.noreply.github.com>
2026-04-30 17:15:42 -0700

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const numSet = new Set(nums);
+        let longestStreak = 0;
+
+        for (const num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let currentNum = num;
+                let currentStreak = 1;
+
+                while (numSet.has(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
