52f05d9c95bff4e605ad9047a7e2d99024ed1408
yeminzaw04 <42385092+yeminzaw04@users.noreply.github.com>
2026-05-06 21:14:47 -0700

    Add: maximum-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-subarray/submission-6.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let windowStart = 0;
+        let largestSumSoFar = -Infinity;
+        let sum = 0;
+        
+        for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+            sum += nums[windowEnd];
+            largestSumSoFar = Math.max(sum, largestSumSoFar);
+
+            if (sum < 0) {
+                sum = 0;
+                windowStart = windowEnd;
+            }
+        }
+        return largestSumSoFar;
+    }
+}
