d287b0a0df6f2ef1375627f5ccbebcc0aced3d1d
yeminzaw04 <42385092+yeminzaw04@users.noreply.github.com>
2026-05-14 00:05:27 -0700

    Update: buy-and-sell-crypto - submission-19

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-19.js
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-19.js
@@ -4,18 +4,33 @@ class Solution {
      * @return {number}
      */
     maxProfit(prices) {
-        let left = 0; // Buy
-        let right = 1; // Sell
-        let maxProfit = 0; // Record profit
+        // let left = 0; // Buy
+        // let right = 1; // Sell
+        // let maxProfit = 0; // Record profit
 
-        while (right < prices.length) {
-            if (prices[left] < prices[right]) {
-                let profit = prices[right] - prices[left];
-                maxProfit = Math.max(maxProfit, profit);
+        // while (right < prices.length) {
+        //     if (prices[left] < prices[right]) {
+        //         let profit = prices[right] - prices[left];
+        //         maxProfit = Math.max(maxProfit, profit);
+        //     } else {
+        //         left = right;
+        //     }
+        //     right++;
+        // }
+
+        // return maxProfit;
+        let windowStart = 0;
+        let maxProfit = 0;
+
+        for (let windowEnd = 1; windowEnd < prices.length; windowEnd++) {
+            if (prices[windowStart] < prices[windowEnd]) {
+                // Sell and Update Profit
+                let profit = prices[windowEnd] - prices[windowStart];
+                maxProfit = Math.max(profit, maxProfit);
             } else {
-                left = right;
+                // Buy
+                windowStart = windowEnd;
             }
-            right++;
         }
 
         return maxProfit;
